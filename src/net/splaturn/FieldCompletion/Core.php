2921b96c39cc0ce3df7bcb4179d7ec5597400aaa
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2020-02-29 04:19:38 +0900

    Implements line completion * Update Composer dependencies

--- src/net/splaturn/FieldCompletion/Core.php
+++ src/net/splaturn/FieldCompletion/Core.php
@@ -14,7 +14,48 @@ use pocketmine\utils\TextFormat;
  * Class Core
  * @package net\splaturn\FieldCompletion
  */
-class Core extends PluginBase{
+class Core extends PluginBase {
+
+  private const OPPOSITE_DAMAGE = [
+    0 => 1,
+    1 => 0,
+    2 => 3,
+    3 => 2,
+    4 => 5,
+    5 => 4,
+    6 => 7,
+    7 => 6,
+    8 => 9,
+    9 => 8,
+    10 => 11,
+    11 => 10,
+    12 => 13,
+    13 => 12,
+    14 => 15,
+    15 => 14
+  ];
+
+  private const OPPOSITE_DAMAGE_X = [
+    0 => 1,
+    1 => 0,
+    4 => 5,
+    5 => 4,
+    8 => 9,
+    9 => 8,
+    12 => 13,
+    13 => 12
+  ];
+
+  private const OPPOSITE_DAMAGE_Z = [
+    2 => 3,
+    3 => 2,
+    6 => 7,
+    7 => 6,
+    10 => 11,
+    11 => 10,
+    14 => 15,
+    15 => 14
+  ];
 
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
@@ -24,7 +65,18 @@ class Core extends PluginBase{
 	  if ($sender instanceof Player) {
       $session = Session::get($sender);
 	    if ($session->checkParameters()) {
-        $session->completion();
+        if (!empty($args[0])) {
+          switch (strtolower($args[0])) {
+            case "point":
+              $session->pointCompletion();
+              break;
+            case "line":
+              $session->lineCompletion();
+              break;
+          }
+        }else {
+          return false;
+        }
       }else {
 	      $sender->sendMessage(TextFormat::RED . "{$this->getPrefix()} 先に回転軸、始点、終点の3つを設定してください");
       }
@@ -36,5 +88,17 @@ class Core extends PluginBase{
 
   public function getPrefix(): string {
 	  return "[{$this->getDescription()->getPrefix()}]";
+  }
+
+  public static function getOppositeSideDamage(int $damage, ?bool $axisX = null): int {
+	  if ($axisX !== null) {
+      if ($axisX) {
+        return self::OPPOSITE_DAMAGE_Z[$damage] ?? $damage;
+      }else {
+        return self::OPPOSITE_DAMAGE_X[$damage] ?? $damage;
+      }
+    }else {
+	    return self::OPPOSITE_DAMAGE[$damage] ?? $damage;
+    }
   }
 }
--- src/net/splaturn/FieldCompletion/Session.php
+++ src/net/splaturn/FieldCompletion/Session.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace net\splaturn\FieldCompletion;
 
+use pocketmine\block\BlockIds;
+use pocketmine\block\EndRod;
+use pocketmine\block\IronTrapdoor;
+use pocketmine\block\Stair;
+use pocketmine\block\StandingBanner;
+use pocketmine\block\Torch;
+use pocketmine\block\Trapdoor;
+use pocketmine\block\WallBanner;
 use pocketmine\math\Vector2;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
@@ -31,9 +39,9 @@ class Session {
   }
 
   /**
-   * 補完を実行
+   * 点対称に補完を実行
    */
-  public function completion() {
+  public function pointCompletion() {
     $rotationAxis = $this->getRotationAxis();
 
     $start = $this->getStart();
@@ -68,7 +76,88 @@ class Session {
     for ($nx = $minN->getX(); $nx <= $maxN->getX(); $nx++) {
       for ($ny = $maxN->getY(); $ny >= $minN->getY(); $ny--) {
         for ($nz = $minN->getZ(); $nz <= $maxN->getZ(); $nz++) {
-          $lev->setBlock(new Vector3($nx, $ny, $nz), $blocks[$count--]);
+          $block = $blocks[$count--];
+          if ($block instanceof Stair || $block instanceof Trapdoor || $block instanceof IronTrapdoor || $block instanceof Torch || $block instanceof WallBanner || $block instanceof StandingBanner || $block instanceof EndRod) {
+            $block->setDamage(Core::getOppositeSideDamage($block->getDamage()));
+          }
+          $lev->setBlock(new Vector3($nx, $ny, $nz), $block);
+        }
+      }
+    }
+    $this->executor->sendMessage("Done!");
+  }
+
+  /**
+   * 線対称に補完を実行
+   */
+  public function lineCompletion() {
+    $rotationAxis = $this->getRotationAxis();
+    $start = $this->getStart();
+    $vec2Start = new Vector2($start->getX(), $start->getZ());
+    $end = $this->getEnd();
+    $vec2End = new Vector2($end->getX(), $end->getZ());
+
+    $diffStart = $vec2Start->subtract($rotationAxis);
+    $diffEnd = $vec2End->subtract($rotationAxis);
+
+    $minX = min($diffStart->x, $diffEnd->x);
+    $minZ = min($diffStart->y, $diffEnd->y);
+
+    if ($minX > $minZ) {
+      $newStart = $diffStart->y * -1 + $rotationAxis->y;
+      $targetStart = new Vector3($start->x, $start->y, $newStart);
+
+      $newEnd = $diffEnd->y * -1 + $rotationAxis->y;
+      $targetEnd = new Vector3($end->x, $end->y, $newEnd);
+    }else {
+      $newStart = $diffStart->x * -1 + $rotationAxis->x;
+      $targetStart = new Vector3($newStart, $start->y, $start->z);
+
+      $newEnd = $diffEnd->x * -1 + $rotationAxis->x;
+      $targetEnd = new Vector3($newEnd, $end->y, $end->z);
+    }
+
+    $lev = $this->executor->getLevel();
+
+    $minO = VectorMath::minComponents($start, $end);
+    $maxO = VectorMath::maxComponents($start, $end);
+    $minN = VectorMath::minComponents($targetStart, $targetEnd);
+    $maxN = VectorMath::maxComponents($targetStart, $targetEnd);
+
+    $blocks = [];
+    for ($x = $minO->getX(); $x <= $maxO->getX(); $x++) {
+      for ($y = $minO->getY(); $y <= $maxO->getY(); $y++) {
+        for ($z = $minO->getZ(); $z <= $maxO->getZ(); $z++) {
+          $blocks[] = $lev->getBlockAt($x, $y, $z);
+        }
+      }
+    }
+
+    $this->executor->sendMessage("Copied!");
+
+    $count = count($blocks) - 1;
+    if ($minX > $minZ) {
+      for ($nx = $maxN->getX(); $nx >= $minN->getX(); $nx--) {
+        for ($ny = $maxN->getY(); $ny >= $minN->getY(); $ny--) {
+          for ($nz = $minN->getZ(); $nz <= $maxN->getZ(); $nz++) {
+            $block = $blocks[$count--];
+            if ($block instanceof Stair || $block instanceof Trapdoor || $block instanceof IronTrapdoor || $block instanceof Torch || $block instanceof WallBanner || $block instanceof StandingBanner || $block instanceof EndRod) {
+              $block->setDamage(Core::getOppositeSideDamage($block->getDamage(), true));
+            }
+            $lev->setBlock(new Vector3($nx, $ny, $nz), $block);
+          }
+        }
+      }
+    }else {
+      for ($nx = $minN->getX(); $nx <= $maxN->getX(); $nx++) {
+        for ($ny = $maxN->getY(); $ny >= $minN->getY(); $ny--) {
+          for ($nz = $maxN->getZ(); $nz >= $minN->getZ(); $nz--) {
+            $block = $blocks[$count--];
+            if ($block instanceof Stair || $block instanceof Trapdoor || $block instanceof IronTrapdoor || $block instanceof Torch || $block instanceof WallBanner || $block instanceof StandingBanner || $block instanceof EndRod) {
+              $block->setDamage(Core::getOppositeSideDamage($block->getDamage(), false));
+            }
+            $lev->setBlock(new Vector3($nx, $ny, $nz), $block);
+          }
         }
       }
     }
